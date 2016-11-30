ac500d5aabc9b1995f732544a36e53faa23e36ec
Kevin Stewart <kevinjstew@gmail.com>
2016-11-30 15:58:30 -0500

    Added Unit Test Files

---
+++ FDSY Distribution.Tests/Controllers/Bars_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FDSY_Distribution.Controllers;
+using FDSY_Distribution.Models;
+
+namespace FDSY_Distribution.Tests.Controllers
+{
+    [TestClass]
+    public class Bars_UnitTest
+    {
+        [TestMethod]
+        public void BarsController_AddAction()
+        {
+            BarsController controller = new BarsController();
+            Bar model = new Bar()
+            {
+               
+            };
+
+        }
+    }
+}
---
+++ FDSY Distribution.Tests/Controllers/Brewaries_UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FDSY_Distribution.Tests.Controllers
+{
+    [TestClass]
+    public class Brewaries_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+        }
+    }
+}
