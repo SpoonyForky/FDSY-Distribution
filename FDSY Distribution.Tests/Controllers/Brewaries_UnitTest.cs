21274368d91a8c83b2c401bd7b1844403ddf6eec
Chris Goguen <Chris Goguen>
2016-12-03 12:38:10 -0500

    Finished Unit Tests for Brewaries

--- FDSY Distribution.Tests/Controllers/Brewaries_UnitTest.cs
+++ FDSY Distribution.Tests/Controllers/Brewaries_UnitTest.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FDSY_Distribution.Models.Repositories;
+using FDSY_Distribution.Models;
+using FDSY_Distribution.Controllers;
+using FDSY_Distribution.Controllers.ApiControllers;
+using System.Web.Mvc;
+using System.Web.Http.Results;
+using System.Collections.Generic;
+using System.Web.Http;
 
 namespace FDSY_Distribution.Tests.Controllers
 {
@@ -7,8 +15,150 @@ namespace FDSY_Distribution.Tests.Controllers
     public class Brewaries_UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
-        { 
+        public void BrewariesController_AddAction()
+        {
+            BreweriesController controller = new BreweriesController(new FakeRepositories.FakeBrewaryRepository());
+
+            Brewery brew = new Brewery()
+            {
+                Name = "Sleeman",
+                Address = "Oakville",
+                YTDSales = 500,
+                BreweryId = 5
+            };
+
+            System.Web.Mvc.RedirectToRouteResult result = controller.Create(brew) as System.Web.Mvc.RedirectToRouteResult;
+
+            Assert.IsNotNull(result);
+
         }
+
+
+
+        [TestMethod]
+        public void BrewariesController_GetOne()
+        {
+            BreweriesController controller = new BreweriesController(new FakeRepositories.FakeBrewaryRepository());
+            ViewResult result = controller.Details(5) as ViewResult;
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void BrewariesController_GetAll()
+        {
+            BreweriesController controller = new BreweriesController(new FakeRepositories.FakeBrewaryRepository());
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void BrewariesController_Delete()
+        {
+            BreweriesController controller = new BreweriesController(new FakeRepositories.FakeBrewaryRepository());
+
+            Brewery brew = new Brewery()
+            {
+                Name = "Molson",
+                Address = "Toronto",
+                YTDSales = 5030,
+                BreweryId = 1
+            };
+
+            System.Web.Mvc.RedirectToRouteResult result = controller.Create(brew) as System.Web.Mvc.RedirectToRouteResult;
+
+            System.Web.Mvc.RedirectToRouteResult deleteResult = controller.DeleteConfirmed(1) as System.Web.Mvc.RedirectToRouteResult;
+
+            System.Web.Mvc.RedirectToRouteResult model = controller.Details(1) as System.Web.Mvc.RedirectToRouteResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(deleteResult);
+            Assert.IsNull(model);
+
+        }
+
+        [TestMethod]
+        public void BrewariesController_Edit()
+        {
+            BreweriesController controller = new BreweriesController(new FakeRepositories.FakeBrewaryRepository());
+
+            Brewery brew = new Brewery()
+            {
+                Name = "Molson",
+                Address = "Toronto",
+                YTDSales = 5030,
+                BreweryId = 1
+            };
+
+            System.Web.Mvc.RedirectToRouteResult result = controller.Create(brew) as System.Web.Mvc.RedirectToRouteResult;
+            System.Web.Mvc.RedirectToRouteResult edit = controller.Edit(brew) as System.Web.Mvc.RedirectToRouteResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(edit);
+
+        }
+
+
+        [TestMethod]
+        public void BreweriesApiController_GetOne()
+        {
+            BreweriesApiController controller = new BreweriesApiController(new FakeRepositories.FakeBrewaryRepository());
+            OkNegotiatedContentResult<Brewery> result = controller.GetBrewery(1) as OkNegotiatedContentResult<Brewery>;
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void BreweriesApiController_GetAll()
+        {
+            BreweriesApiController controller = new BreweriesApiController(new FakeRepositories.FakeBrewaryRepository());
+            var result = controller.GetBreweries() as List<Brewery>;
+            Assert.IsNotNull(result);
+
+        }
+
+
+        [TestMethod]
+        public void BrewariesApiController_Delete()
+        {
+            BreweriesApiController controller = new BreweriesApiController(new FakeRepositories.FakeBrewaryRepository());
+
+            Brewery brew = new Brewery()
+            {
+                Name = "Molson",
+                Address = "Toronto",
+                YTDSales = 5030,
+                BreweryId = 7
+            };
+            System.Web.Http.Results.CreatedAtRouteNegotiatedContentResult<Brewery> result = controller.PostBrewery(brew) as System.Web.Http.Results.CreatedAtRouteNegotiatedContentResult<Brewery>;
+
+            OkNegotiatedContentResult<Brewery> deleteResult = controller.DeleteBrewery(7) as OkNegotiatedContentResult<Brewery>;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(deleteResult);
+
+        }
+
+        [TestMethod]
+        public void BrewariesApiController_Edit()
+        {
+            BreweriesApiController controller = new BreweriesApiController(new FakeRepositories.FakeBrewaryRepository());
+
+            Brewery brew = new Brewery()
+            {
+                Name = "Molson",
+                Address = "Toronto",
+                YTDSales = 5030,
+                BreweryId = 7
+            };
+            System.Web.Http.Results.CreatedAtRouteNegotiatedContentResult<Brewery> result = controller.PostBrewery(brew) as System.Web.Http.Results.CreatedAtRouteNegotiatedContentResult<Brewery>;
+            IHttpActionResult edit = controller.PutBrewery(10, brew) as IHttpActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(edit);
+
+        }
+
+
     }
 }
