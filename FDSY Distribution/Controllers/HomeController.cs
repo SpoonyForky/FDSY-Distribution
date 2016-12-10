7f229ac03c66688272d5fe3102e3f2c26dbf5c22
Kevin Stewart <kevinjstew@gmail.com>
2016-12-09 22:02:02 -0500

    Added homepage with a random beer

--- FDSY Distribution/Controllers/HomeController.cs
+++ FDSY Distribution/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FDSY_Distribution.Models;
+using FDSY_Distribution.Models.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +8,29 @@ using System.Web.Mvc;
 
 namespace FDSY_Distribution.Controllers
 {
+    /// <summary>
+    /// Controller for Home
+    /// </summary>
     public class HomeController : Controller
     {
+
+        private IRepository<Beer> repo;
+
+        public HomeController(IRepository<Beer> _repo)
+        {
+            this.repo = _repo;
+        }
+
+        public HomeController() : this(new BeerRepository()) { }
+
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            Random rnd = new Random();
+            List<Beer> _beers = repo.Get().ToList();
+            int beerIndex = rnd.Next(0, _beers.Count);
 
-            return View();
+
+            return View(_beers[beerIndex]);
         }
     }
 }
